24f567bf2fcfc4d6d755fdd733331854e584c1c9
rfeldgeorge <rosenfeldgeorge190@gmail.com>
2023-11-30 19:47:26 +0100

    changing import names

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,5 +1,5 @@
-import { prisma } from "@/lib/db";
-import { stripe } from "@/lib/stripe";
+import { prisma } from "../../../lib/db";
+import { stripe } from "../../../lib/stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
